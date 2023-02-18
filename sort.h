8b5b18a1c1d5c4c82f8e45143e68029cfcf5cba7
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-02-18 11:30:16 +0200

    update prototypes

--- sort.h
+++ sort.h
@@ -19,11 +19,14 @@ typedef struct listint_s
 } listint_t;
 
 void swap(int *a, int *b);
+void swap2(int *array, size_t size, int *a, int *b);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int lomuto(int *array, int low, int high, size_t size);
+void quicksort_recursion(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
